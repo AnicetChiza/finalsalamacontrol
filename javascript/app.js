4ac6c36418f53516f8ed2c8104ac2ed35084af80
unknown <chizaanicet@gmail.com>
2023-11-12 11:57:36 +0200

    add to my repository

--- javascript/app.js
+++ javascript/app.js
@@ -1,3 +1,18 @@
+//Cette partie concerne le header de nos differentes pages
+const menu = document.querySelector('.menu-icons');
+const exit = document.querySelector('.menu-container');
+
+menu.addEventListener('click', () =>{
+    menu.classList.toggle('menu-mobile');
+    exit.classList.toggle('menu-mobile');
+})
+
+document.querySelectorAll('.nav-links').forEach(n => n.addEventListener('click', () =>{
+    menu.classList.remove('menu-mobile');
+    exit.classList.remove('menu-mobile');
+}))
+
+//Cette partie concerne le bouton top dans la page d'accueil qui doit envoyer les users au debut de la page
 const scroll = document.querySelector('.top');
 scroll.addEventListener('click', () =>{
     window.scrollTo({
