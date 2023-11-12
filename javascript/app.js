34960a1b4255cc60594724c4e3116ba5027b6f00
unknown <chizaanicet@gmail.com>
2023-11-12 15:20:28 +0200

    add to my repository

--- javascript/app.js
+++ javascript/app.js
@@ -12,6 +12,28 @@ document.querySelectorAll('.nav-links').forEach(n => n.addEventListener('click',
     exit.classList.remove('menu-mobile');
 }))
 
+//Cette partie concerne le dossier bureau central a la page Dashboard
+document.addEventListener("DOMContentLoaded", function () {
+    const counters = document.querySelectorAll(".counter");
+    const maxCounts = [304, 132, 172];
+    const intervals = [];
+
+    function updateCounter(index, maxCount) {
+        return function () {
+            const currentValue = parseInt(counters[index].textContent);
+            if (currentValue < maxCount) {
+                counters[index].textContent = currentValue + 1;
+            } else {
+                clearInterval(intervals[index]);
+            }
+        };
+    }
+
+    counters.forEach((counter, index) => {
+        intervals[index] = setInterval(updateCounter(index, maxCounts[index]), 10);
+    });
+});
+
 //Cette partie concerne le bouton top dans la page d'accueil qui doit envoyer les users au debut de la page
 const scroll = document.querySelector('.top');
 scroll.addEventListener('click', () =>{
